b4f548cb0d3e286e08f804a673d3118509f4b17e
Pitou Teng <pitouteng@ymail.com>
2018-03-28 23:21:15 -0400

    a

--- js/script.js
+++ js/script.js
@@ -33,60 +33,60 @@ counterCursor++;
 }
 setInterval(blinkCursor, 500); // Time in milliseconds
 
-// slide
-var myIndex = 1;
-carousel();
+// // slide ************************
 
-function carousel() {
-var i;
-var x = document.getElementsByClassName("slide");
-// alert("slide length is: " + x.length);
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-}
 
-if (myIndex == 6) {myIndex = 1} 
-// slide animation
-for (var j = 0; j < (x.length-1); j++) {
-    x[j].classList.add('w3-animate-right');
-    x[j].classList.remove('w3-animate-left');
-}
-for (var j = (x.length-1); j > myIndex; j--) {
-    x[myIndex].classList.add('w3-animate-right');
-    x[myIndex].classList.remove('w3-animate-left');
-}
-// for (var j = 1; j < myIndex; j++) {
-// 	x[myIndex].classList.add('w3-animate-left');
-// 	x[myIndex].classList.remove('w3-animate-right');
+
+
+
+
+// var myIndex = 1;
+// carousel();
+
+// function carousel() {
+// var i;
+// var x = document.getElementsByClassName("slide");
+// // alert("slide length is: " + x.length);
+// for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+// }
+
+// if (myIndex == 6) {myIndex = 1} 
+// // slide animation
+// for (var j = 0; j < (x.length-1); j++) {
+//     x[j].classList.add('w3-animate-right');
+//     x[j].classList.remove('w3-animate-left');
 // }
 // for (var j = (x.length-1); j > myIndex; j--) {
-// 	x[myIndex].classList.add('w3-animate-right');
-// 	x[myIndex].classList.remove('w3-animate-left');
+//     x[myIndex].classList.add('w3-animate-right');
+//     x[myIndex].classList.remove('w3-animate-left');
 // }
 
-// display
-if(myIndex >= 2){
-    x[myIndex-1].style.display = "block";  
-    x[myIndex-1].style.height = "80px"; 
-}
-else {
-    x[0].style.display = "block"; 
-}
 
-x[myIndex].style.display = "block";  
-x[myIndex].style.height = "120px"; 
 
-if(myIndex < 6){
-    x[myIndex+1].style.display = "block";
-    x[myIndex+1].style.height = "80px";  
-}
-else {
-    x[6].style.display = "block"; 
-}
+// // display
+// if(myIndex >= 2){
+//     x[myIndex-1].style.display = "block";  
+//     x[myIndex-1].style.height = "80px"; 
+// }
+// else {
+//     x[0].style.display = "block"; 
+// }
 
-myIndex++; 
-setTimeout(carousel, 3000);
-}
+// x[myIndex].style.display = "block";  
+// x[myIndex].style.height = "120px"; 
 
-// Change image every 2 seconds
+// if(myIndex < 6){
+//     x[myIndex+1].style.display = "block";
+//     x[myIndex+1].style.height = "80px";  
+// }
+// else {
+//     x[6].style.display = "block"; 
+// }
+
+// myIndex++; 
+// setTimeout(carousel, 3000);
+// }
+
+// // // Change image every 2 seconds
 
